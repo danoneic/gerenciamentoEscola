14678fc7d7af5845a502a378f59e167f59cd0d63
danoneic <danone_ic@hotmail.com>
2013-11-24 17:47:46 -0200

    modif

--- LangSchool/src/java/messages/Gmessages.java
+++ LangSchool/src/java/messages/Gmessages.java
@@ -58,6 +58,21 @@ public class Gmessages {
         
         context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Preencha os dados obrigatórios!", "Pois é"));
     }
+    public void idadeInvalida(ActionEvent ae) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Idade inválida!", "Pois é"));
+    } 
+    public void dataInvalida(ActionEvent ae) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Data de nascimento inválida!", "Pois é"));
+    } 
+        public void rgExiste(ActionEvent ae) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"RG Já existe!", "Pois é"));
+    } 
     public void falhaLogin(ActionEvent ae) {
         FacesContext context = FacesContext.getCurrentInstance();
         
